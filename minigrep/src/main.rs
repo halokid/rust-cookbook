786a724ef45adc18f31fdf66adcd261199c425cc
r0x <r00txxyyzz@gmail.com>
2020-09-22 16:38:11 +0800

    doing minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,15 +1,13 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
+use minigrep::run;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
   println!("{:?}", args);
 
-//  let query = &args[1];
-//  let filename = &args[2];
-//  let config = parse_config(&args);
-//  let config = Config::new(&args);
   let config = Config::new(&args).unwrap_or_else(|err| {
     println!("Problem parsing arguments: {}", err);
     process::exit(1);
@@ -18,52 +16,13 @@ fn main() {
   println!("Searching for {}", config.query);
   println!("In file {}", config.filename);
 
-  let contents = fs::read_to_string(config.filename)
-                        .expect("Something went wrong reading the file");
-  println!("With text:\n{}", contents);
-}
-
-struct Config {
-  query:      String,
-  filename:   String,
-}
-
-impl Config {
-  fn new(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() < 3 {
-      return Err("not enouht arguments");
-    }
-
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Ok(Config { query, filename })
+//  run(config);      // config返回是一个Result， 如果不处理Result返回的Err，会产生一个告警
+  if let Err(e) = run(config) {
+    println!("Application error: {}", e);
+    process::exit(1);
   }
 }
 
 
-//impl Config {
-//  fn new(args: &[String]) -> Config {
-//    let query = args[1].clone();
-//    let filename = args[2].clone();
-//
-//    Config { query, filename }
-//  }
-//}
-
-
-//fn parse_config(args: &[String]) -> (&str, &str) {
-//  let query = &args[1];
-//  let filename = &args[2];
-//
-//  (query, filename)
-//}
-
-//fn parse_config(args: &[String]) -> Config {
-//  let query = args[1].clone();
-//  let filename = args[2].clone();
-//
-//  Config { query, filename }
-//}
 
 
