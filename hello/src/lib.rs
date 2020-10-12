5f9a346db41326fc43fb3505b44439b2f13e15f6
r0x <r00txxyyzz@gmail.com>
2020-10-12 15:12:02 +0800

    multi-thread web server

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,7 +1,20 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+  threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
+  /// 创建线程池。
+    ///
+    /// 线程池中线程的数量。
+    ///
+    /// # Panics
+    ///
+    /// `new` 函数在 size 为 0 时会 panic。
   pub fn new(size: usize) -> ThreadPool {
+    assert!(size > 0);
+
     ThreadPool
   }
 
