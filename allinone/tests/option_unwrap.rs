ecf27026c7fb9c3b67907b8dbea94de753452493
r0x <r00txxyyzz@gmail.com>
2020-11-13 20:25:55 +0800

    option & unwrap

---
+++ allinone/tests/option_unwrap.rs
@@ -0,0 +1,14 @@
+
+fn give_commoner(gift: Option<&str>) {
+  match gift {
+    Some("snake") => println!("Yuck, fuck snake"),
+    Some(inner)   => println!("{}? how nice", inner),
+    None          => println!("No gift?"),
+  }
+}
+
+
+#[test]
+fn test_option_unwrap() {
+
+}
