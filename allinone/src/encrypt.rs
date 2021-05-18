5e12ce7b00d70326b04c7447ab61da1edf00b9d3
mm jxx <r00txxyyzz@gmail.com>
2021-05-18 23:53:32 +0800

    base64 decode

--- allinone/src/encrypt.rs
+++ allinone/src/encrypt.rs
@@ -47,7 +47,8 @@ pub fn comm1() {
   let res = hex_to_base64(foo_hex);
   println!("res 3 ----- {}", res);
 
-  let res1 = res.from_base64().unwrap();
+  let res1 = res.from_base64();
+  let res1 = base64::decode(res).unwrap();      // base64 decode
   println!("res 4 ----- {}", res);
 }
 
