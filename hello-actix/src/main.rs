a8829c3853c5a61edc23b11cc09fae10ad8bf7ff
r0x <r00txxyyzz@gmail.com>
2021-05-20 19:00:41 +0800

    web api actix

--- hello-actix/src/main.rs
+++ hello-actix/src/main.rs
@@ -1,4 +1,7 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder,
+                middleware::Logger, Result};
+use env_logger::Env;
+use serde::{Deserialize, Serialize};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -20,8 +23,24 @@ async fn index() -> impl Responder {
 
 // this struct represents state
 struct AppState {
-  app_name:   String,
+  app_name: String,
 }
+
+// todo: 返回json
+#[derive(Serialize, Deserialize)]
+struct MyObj {
+  name: String,
+}
+
+#[get("/json/{name}")]
+async fn resp_json(obj: web::Path<MyObj>) -> Result<HttpResponse> {
+  Ok(
+    HttpResponse::Ok().json(MyObj {
+      name: obj.name.to_string(),
+    })
+  )
+}
+
 
 #[get("/state")]
 async fn state(data: web::Data<AppState>) -> String {
@@ -31,6 +50,7 @@ async fn state(data: web::Data<AppState>) -> String {
 
 #[actix_web::main]          // 这是一个注解, 类似java的@
 async fn main() -> std::io::Result<()> {
+  env_logger::from_env(Env::default().default_filter_or("info")).init();
   HttpServer::new(|| {
     App::new()
       .service(hello)
@@ -38,15 +58,19 @@ async fn main() -> std::io::Result<()> {
       .route("/hey", web::get().to(manual_hello))
       // 自定义handler上下文范围
       .service(
+        // todo: 这样访问 http://127.0.0.1:8080/app/index.html
         web::scope("/app")
           .route("/index.html", web::get().to(index)),
       )
 
       // 定义state
-      .data(AppState{
+      .data(AppState {
         app_name: String::from("Actix-web"),
       })
       .service(state)
+      .wrap(Logger::default())
+    // .wrap(Logger::new("%a %{User-Agent}i"))
+      .service(resp_json)     //
   })
     .bind("127.0.0.1:8080")?
     .run()
