95ded4672efe40e8093827e97b47602ae9442c06
r0x <r00txxyyzz@gmail.com>
2021-07-23 18:51:34 +0800

    doing 2

---
+++ tokio_tutorial/my-redis/example/hello_redis.rs
@@ -0,0 +1,50 @@
+use mini_redis::{client, Result};
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+  // Open a connection to the mini-redis address.
+  let mut client = client::connect("127.0.0.1:6379").await?;
+
+  // Set the key "hello" with value "world"
+  client.set("hello", "world".into()).await?;
+
+  // Get key "hello"
+  let result = client.get("hello").await?;
+
+  println!("got value from the server; result={:?}", result);
+
+
+  // ----------------------------------------------------
+  /*
+  // todo: 调用 `say_world()`， 但是并不执行函数里面的逻辑实体
+  let op = say_world();
+  println!("hello");
+  // todo: 在 op 对象上调用 `.await`, 开始执行 `say_world()`
+  op.await;
+  */
+
+  Ok(())
+}
+
+async fn say_world() {
+  println!("world");
+}
+
+// todo: 一个异步函数
+#[tokio::main]
+async fn a_sync_fn() {
+  println!("i am async fn");
+}
+
+// todo: 上面的异步函数的本质
+fn a_sync_fn_real() {
+  // 获取一个线程 runtime
+  let mut rt = tokio::runtime::Runtime::new().unwrap();
+  rt.block_on(async {
+    println!("i am async fn");
+  })
+}
+
+
+
+
--- tokio_tutorial/my-redis/src/main.rs
+++ tokio_tutorial/my-redis/src/main.rs
@@ -1,25 +1,54 @@
-use mini_redis::{ client, Result };
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+use tokio::task;
 
 #[tokio::main]
-pub async fn main() -> Result<()> {
-  // open a connection to the redis address
-  let mut client = client::connect("127.0.0.1:6379").await?;
-  client.set("hello", "world".into()).await?;
-  let result = client.get("hello").await?;
-  println!("got value from server; result={:?}", result);
+async fn main() {
 
-  // ----------------------------------------------------
-  // todo: 调用 `say_world()`， 但是并不执行函数里面的逻辑实体
-  let op = say_world();
-  println!("hello");
-  // todo: 在 op 对象上调用 `.await`, 开始执行 `say_world()`
-  op.await;
+  // todo: spawn 返回值
+  // todo: 有些传递的 async 语句块是具有返回值的，调用者通过 JoinHandle 的 .await 来获取其返回值，
+  let handle = tokio::spawn(async {
+    "spawn返回的值"
+  });
+  let out = handle.await.unwrap();
+  println!("tokio spawn返回值: {}", out);
 
-  Ok(())
+  // todo: spawn怎么引用 外部线程的变量
+  let v = vec![1, 2, 3];
+  task::spawn(async move {
+    println!("task spawn");
+    println!("v -------- {:?}", v);
+  });
+
+  // todo: -------------- 并发处理网络连接 --------------------
+  let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+  // todo: 非并发处理
+  // loop {
+  //   let (socket, _) = listener.accept().await.unwrap();
+  //   process(socket).await;
+  // }
+
+  // todo: 并发处理， 事实上，Tokio 能够在单线程中并发运行非常多的任务
+  loop {
+    let (socket, _) = listener.accept().await.unwrap();
+    // A new task is spqwned for each inbound socket. the socket is
+    // moved to the new task and processed there.
+    tokio::spawn(async move{
+      process(socket).await;
+    });
+  }
+
 }
 
-async fn say_world() {
-  println!("world");
-}
+async fn process(socket: TcpStream) {
+  let mut connection = Connection::new(socket);
+  if let Some(frame) = connection.read_frame().await.unwrap() {
+    println!("获得redis数据frame: {:?}", frame);
 
+    // 假如服务端 response是一个error
+    let response = Frame::Error("未执行".to_string());
+    connection.write_frame(&response);
+  }
+}
 
