7ca28dd43efd39413c7657d034ccd884c828f59b
mm jxx <r00txxyyzz@gmail.com>
2021-07-26 23:09:11 +0800

    doing 3

--- tokio_tutorial/my-redis/src/main.rs
+++ tokio_tutorial/my-redis/src/main.rs
@@ -1,10 +1,30 @@
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
 use tokio::task;
+use std::rc::Rc;
+use tokio::task::yield_now;
+use mini_redis::Command::{self, Get, Set};
+use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() {
 
+  // +++++++++++++++++++++ Tutorial 02 +++++++++++++++++++++++
+  tokio::spawn(async {
+    // The scope forces `rc` to drop before `.await`.
+    {
+      let rc = Rc::new("hello");
+      // yield_now().await;
+      println!("rc: {}", rc);
+    }
+
+    // `rc` is no longer used. It is **not** persisted when
+    // the task yields to the scheduler
+    yield_now().await;
+  });
+
+
+  // +++++++++++++++++++++ Tutorial 01 +++++++++++++++++++++++
   // todo: spawn 返回值
   // todo: 有些传递的 async 语句块是具有返回值的，调用者通过 JoinHandle 的 .await 来获取其返回值，
   let handle = tokio::spawn(async {
@@ -15,7 +35,7 @@ async fn main() {
 
   // todo: spawn怎么引用 外部线程的变量
   let v = vec![1, 2, 3];
-  task::spawn(async move {
+  task::spawn(async move {    //todo: 这个和 tokio::spawn是一样的
     println!("task spawn");
     println!("v -------- {:?}", v);
   });
@@ -34,13 +54,13 @@ async fn main() {
     let (socket, _) = listener.accept().await.unwrap();
     // A new task is spqwned for each inbound socket. the socket is
     // moved to the new task and processed there.
-    tokio::spawn(async move{
+    tokio::spawn(async move {
       process(socket).await;
     });
   }
-
 }
 
+/*
 async fn process(socket: TcpStream) {
   let mut connection = Connection::new(socket);
   if let Some(frame) = connection.read_frame().await.unwrap() {
@@ -51,4 +71,39 @@ async fn process(socket: TcpStream) {
     connection.write_frame(&response);
   }
 }
+ */
+
+async fn process(socket: TcpStream) {
+  let mut db = HashMap::new();
+  let mut connection = Connection::new(socket);
+
+  while let Some(frame) = connection.read_frame().await.unwrap() {
+    let response = match Command::from_frame(frame).unwrap() {
+      Set(cmd) => {
+        // todo: HashMap的 insert操作要先放上来， 用来声明HashMap的 k, v 的变量类型
+        db.insert(cmd.key().to_string(), cmd.value().to_vec());
+        Frame::Simple("OK".to_string())
+      },
+      Get(cmd) => {
+        if let Some(value) = db.get(cmd.key()) {
+          Frame::Bulk(value.clone().into())
+        } else {
+          Frame::Null
+        }
+      },
+      cmd => panic!("没有执行命令 {:?}", cmd),
+    };
+
+    connection.write_frame(&response).await.unwrap();
+  }
+}
+
+
+
+
+
+
+
+
+
 
