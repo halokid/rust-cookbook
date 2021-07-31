935f0cfad516b38ce407a2ec30202350a443be22
mm jxx <r00txxyyzz@gmail.com>
2021-07-31 15:14:13 +0800

    tokio tuturial done

--- tokio_tutorial/my-redis/example/mytest.rs
+++ tokio_tutorial/my-redis/example/mytest.rs
@@ -21,11 +21,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
     // tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
     println!("concurrence doing 1");
-  });
+  // });      // todo: 这样的话，线程还是会执行，但是不会阻塞主线程，也就是主线程不用等待
+  }).await;   // todo: 这样的话，这里就会等待这个线程的执行
 
   // concurr_do().await;
   let act = concurr_do();     // todo: 不会执行
-  act.await;    // todo: 执行
+  act.await;    // todo: 等待线程执行
 
   println!("DONE");
 
