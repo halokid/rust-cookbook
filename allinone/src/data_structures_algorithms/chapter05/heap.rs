78484124f378daa2ded7335206c21d221ad4dfba
r0x <r00txxyyzz@gmail.com>
2022-05-16 18:23:22 +0800

    add heap

---
+++ allinone/src/data_structures_algorithms/chapter05/heap.rs
@@ -0,0 +1,9 @@
+
+pub struct MessageChecker {
+  pub length:  usize,
+  heap: Vec<Box<MessageNotification>>,
+}
+
+impl MessageChecker {
+
+}
--- allinone/src/data_structures_algorithms/mod.rs
+++ allinone/src/data_structures_algorithms/mod.rs
@@ -1,3 +1,4 @@
 pub mod chapter01;
 pub mod chapter03;
 pub mod chapter04;
+pub mod chapter05;
