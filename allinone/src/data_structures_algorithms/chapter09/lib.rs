15ae90a046db4cbb4720600248fb25df73588dc0
r0x <master@xx.com>
2022-07-14 13:57:49 +0800

    sorting sample

--- allinone/src/data_structures_algorithms/chapter09/lib.rs
+++ allinone/src/data_structures_algorithms/chapter09/lib.rs
@@ -18,6 +18,38 @@ pub fn bubble_sort<T: PartialOrd + Clone>(collection: &[T]) -> Vec<T> {
   result
 }
 
+pub fn shell_sort<T: PartialOrd + Clone>(collection: &[T]) -> Vec<T> {
+  let n = collection.len();
+  let mut gap = n / 2;
+  let mut result: Vec<T> = collection.into();
+
+  while gap > 0 {
+    // todo: `i` first equal `gap`
+    for i in gap..n {
+      let temp = result[i].clone();
+
+      // todo: `j` first equal `i`
+      let mut j = i;
+      while j >= gap && result[j - gap] > temp {
+        result[j] = result[j - gap].clone();
+        j -= gap;
+      }
+      result[j] = temp;
+    }
+    gap /= 2
+  }
+  result
+}
+
+pub fn heap_sort<T: PartialOrd + Clone + Ord>(collection: &[T]) -> Vec<T> {
+  let mut heap = binary_heap::BinaryHeap::new();
+  for c in collection {
+    heap.push(c.clone());
+  }
+  heap.into_sorted_vec()
+}
+
+
 
 
 
