b60c35ebe2d8bf681ee22f2107a8a1c779cc9441
r0x <master@xx.com>
2022-09-07 18:57:03 +0800

    coding life cycle

---
+++ allinone/src/course_rs/life_cycle/c3.rs
@@ -0,0 +1,18 @@
+
+#[derive(Debug)]
+struct Foo;
+
+impl Foo {
+  fn mutate_and_share(&mut self) -> &Self {
+    &*self
+  }
+
+  fn share(&self) {}
+}
+
+pub fn comm() {
+  let mut foo = Foo;
+  let loan = foo.mutate_and_share();
+  foo.share();
+  println!("{:?}", loan);
+}
