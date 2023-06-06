b623a7d39f0c900d1030fd1f3c40e6aea7afd3af
r0x <r0x@r0x.org>
2023-06-06 18:39:00 +0800

    webserv thread pool version done.

---
+++ allinone/src/course_rs/practice/hello/backup/single_thread.rs
@@ -0,0 +1,107 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+
+
+fn main() {
+  let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+  for stream in listener.incoming() {
+    let stream = stream.unwrap();
+
+    handle_connection(stream);
+  }
+}
+
+/*
+fn handle_connection(mut stream: TcpStream) {
+  let buf_reader = BufReader::new(&mut stream);
+  let http_request: Vec<_> = buf_reader
+    .lines()
+    .map(|result| result.unwrap())
+    .take_while(|line| !line.is_empty())
+    .collect();
+
+  let status_line = "HTTP/1.1 200 OK";
+  let contents = fs::read_to_string("hello.html").unwrap();
+  let length = contents.len();
+
+  let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+  stream.write_all(response.as_bytes()).unwrap();
+}
+ */
+
+/*
+fn handle_connection(mut stream: TcpStream) {
+  let buf_reader = BufReader::new(&mut stream);
+  let request_line = buf_reader.lines().next()
+    .unwrap().unwrap();
+
+  if request_line == "GET / HTTP/1.1" {
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!(
+      "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+  } else {
+    let status_line = "HTTP/1.1 404 NOT FOUND";
+    let contents = fs::read_to_string("404.html").unwrap();
+    let length = contents.len();
+
+    let response = format!(
+      "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+  }
+}
+ */
+
+
+fn handle_connection(mut stream: TcpStream) {
+  let buf_reader = BufReader::new(&mut stream);
+  let request_line = buf_reader.lines().next()
+    .unwrap().unwrap();
+
+  let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+    ("HTTP/1.1 200 OK", "hello.html")
+  } else {
+    ("HTTP/1.1 404 NOT FOUND", "404.html")
+  };
+
+  // mock slow request for the single thread webserv
+  let (status_line, filename) = match &request_line[..] {
+    "GET / HTTP/1.1" =>  ("HTTP/1.1 200 OK", "hello.html"),
+
+    "GET /sleep HTTP/1.1" => {
+      thread::sleep(Duration::from_secs(5));
+      ("HTTP/1.1 200 OK", "hello.html")
+    },
+
+    _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+  };
+
+  let contents = fs::read_to_string(filename).unwrap();
+  let length = contents.len();
+
+  let response = format!(
+    "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+  );
+
+  stream.write_all(response.as_bytes()).unwrap();
+}
+
+
+
+
+
+
