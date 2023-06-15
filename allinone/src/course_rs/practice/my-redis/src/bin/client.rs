907d3757f6892c442c4deb792b30bd2f47bbfaab
r0x <r0x@r0x.org>
2023-06-15 14:06:20 +0800

    coding my-redis client

--- allinone/src/course_rs/practice/my-redis/src/bin/client.rs
+++ allinone/src/course_rs/practice/my-redis/src/bin/client.rs
@@ -1,7 +1,87 @@
+use std::fmt::Alignment::Left;
+use bytes::Bytes;
 use mini_redis::client;
+use tokio::sync::{mpsc, oneshot};
+
+#[derive(Debug)]
+enum Command {
+  Get {
+    key: String,
+    resp: Responder<Option<Bytes>>,
+  },
+  Set {
+    key: String,
+    val: Bytes,
+    resp: Responder<T>,
+  },
+}
+
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 #[tokio::main]
 async fn main() {
+
+  let (tx, mut rx) = mpsc::channel(32);
+  let tx2 = tx.clone();
+
+  let manager = tokio::spawn(async move {
+    let mut clientx = client::connect("127.0.0.1:6379").await.unwrap();
+
+    while let Some(cmd) = rx.recv().await {
+      match cmd {
+        Command::Get {key, resp} => {
+          let res = clientx.get(&key).await;
+          let _ = resp.send(res);
+        },
+
+        Command::Set {key, val, resp} => {
+          let res = clientx.set(&key, val).await;
+          let _ = resp.send(res);
+        }
+      }
+    }
+  });
+
+  let t1 = tokio::spawn(async move {
+    let (resp_tx, resp_rx) = oneshot::channel();
+    let cmd = Command::Get {
+      key: "foo".to_string(),
+      resp: resp_tx,
+    };
+
+    // send SET command
+    if tx.send(cmd).await.is_err() {
+      eprint!("connection task shutdown");
+      return;
+    }
+
+    // Await the response
+    let res = resp_rx.await;
+    println!("GOT (Set) = {:?}", res);
+  });
+
+  let t2 = tokio::spawn(async move {
+    let (resp_tx, resp_rx) = oneshot::channel();
+    let cmd = Command::Set {
+      key: "foo2".to_string(),
+      val: "bar".into(),
+      resp: resp_tx,
+    };
+
+    if tx2.send(cmd).await.is_err() {
+      eprint!("connection task shutdown");
+      return;
+    }
+
+    let res = resp_rx.await;
+    println!("GOT (Set) = {:?}", res);
+  });
+
+  t1.await.unwrap();
+  t2.await.unwrap();
+  manager.await.unwrap();
+
+  /*    TODO: wrong sample
   let mut clientx = client::connect("127.0.0.1:6379")
     .await.unwrap();
 
@@ -15,4 +95,9 @@ async fn main() {
 
   t1.await.unwrap();
   t2.await.unwrap();
+   */
 }
+
+
+
+
